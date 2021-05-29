9dd6780b34584bbc0fb2a5a0bc643ed99143f68f
KatherinMichel <katherinmichel88@gmail.com>
2021-05-30 00:41:47 +0200

    City API

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ function displayTemperature(response){
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
-    
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML= response.data.weather[0].description;
@@ -38,6 +38,7 @@ function displayTemperature(response){
 }
 
 let apiKey ="42cd377212708e14d030c89f051bf208";
-let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=Manila&appid=${apiKey}&units=metric`;
+let city = "Manila";
+let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);

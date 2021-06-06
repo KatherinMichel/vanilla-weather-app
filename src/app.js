444dd6d7fe1933e197775a28f719f0a6322174e2
KatherinMichel <katherinmichel88@gmail.com>
2021-06-06 13:44:05 +0200

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -20,35 +20,55 @@ function formatDate(timestamp){
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+}
+
+function displayForecast(response) {
+    let forecast = response.data.daily;
+    
     let forecastElement = document.querySelector("#forecast");
 
-    let days = ["Thu", "Fri", "Sat", "Sun"];
+
     let forecastHTML = `<div class="row">`;
-    days.forEach(function(day) {
-        forecastHTML = forecastHTML +
+    forecast.forEach(function(forecastDay, index) {
+        if (index < 6) {
+        forecastHTML = 
+        forecastHTML +
                 `
                     <div class="col-2">
-                        <div class="weather-forecast-date">${day}</div>
+                        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
                         <img 
-                        src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                        src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
                         alt=""
                         width="42"
                         />
                         <div class="weather-forecast-temperature">
                             <span class="weather-forecast-temperature-max">
-                        18° 
+                        ${Math.round(forecastDay.temp.max)}° 
                         </span>
                         <span class="weather-forecast-temperature-min">
-                        12°
+                        ${Math.round(forecastDay.temp.min)}°
                         </span>
                         </div>
                         </div>
                 `;
+                }
     })
       
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey ="42cd377212708e14d030c89f051bf208";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response){
@@ -72,6 +92,9 @@ function displayTemperature(response){
         "src",
         `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
+
+    getForecast(response.data.coord);
+
 }
 
 function search(city){
@@ -116,4 +139,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Munich");
-displayForecast();
